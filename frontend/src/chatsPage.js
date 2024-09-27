21c97b4e9f47fbd93c43f7a819af58a589bc4d40
Luswata Andrew <luswataandrew190@gmail.com>
2024-09-27 03:30:19 +0300

    Add Jest and Babel configuration, implement tests for AuthPage and ChatsPage components

--- frontend/src/chatsPage.js
+++ frontend/src/chatsPage.js
@@ -1,16 +1,16 @@
+import React from "react";
 import { PrettyChatWindow } from "react-chat-engine-pretty";
 
-const ChatsPage = (props) => {
-  return (
-    <div style={{ height: "100vh", width: "100vw" }}>
-      <PrettyChatWindow
-        projectId={process.env.REACT_APP_CHAT_ENGINE_PROJECT_ID}
-        username={props.user.username} 
-        secret={props.user.secret} 
-        style={{ height: "100%" }}
-      />
-    </div>
-  );
-};
+const ChatsPage = ({ user = { username: "", secret: "" } }) => (
+  <div style={{ height: "100vh", width: "100vw" }}>
+    <PrettyChatWindow
+      projectId={process.env.REACT_APP_CHAT_ENGINE_PROJECT_ID}
+      username={user.username}
+      secret={user.secret}
+      style={{ height: "100%" }}
+      data-testid="pretty-chat-window"
+    />
+  </div>
+);
 
 export default ChatsPage;
---
+++ frontend/src/tests-frontend/authPage.test.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import AuthPage from "../authPage";
+
+describe("AuthPage Component", () => {
+  it("renders the login form", () => {
+    render(<AuthPage onAuth={jest.fn()} />);
+
+    expect(screen.getByPlaceholderText(/login username/i)).toBeInTheDocument();
+    expect(screen.getByPlaceholderText(/login password/i)).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: /log in/i })).toBeInTheDocument();
+  });
+
+  it("renders the sign up form", () => {
+    render(<AuthPage onAuth={jest.fn()} />);
+
+    expect(
+      screen.getByPlaceholderText(/sign up username/i)
+    ).toBeInTheDocument();
+    expect(
+      screen.getByPlaceholderText(/sign up password/i)
+    ).toBeInTheDocument();
+    expect(screen.getByPlaceholderText(/email/i)).toBeInTheDocument();
+    expect(screen.getByPlaceholderText(/first name/i)).toBeInTheDocument();
+    expect(screen.getByPlaceholderText(/last name/i)).toBeInTheDocument();
+    expect(
+      screen.getByRole("button", { name: /sign up/i })
+    ).toBeInTheDocument();
+  });
+});
